af6ba9ed3ab7da1d82f97a4969e9568eb7462d67
Jeme <aime.urquieta@outlook.com>
2019-03-09 18:55:52 -0600

    redux form set up

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,12 +9,12 @@ import Header from "./Header";
 
 const App = () => {
   return (
-    <div className="container">
+    <div className="ui container">
       <BrowserRouter>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
-          <Route path="/streams/create" exact component={StreamCreate} />
+          <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/edit" exact component={StreamEdit} />
           <Route path="/streams/show" exact component={StreamShow} />
