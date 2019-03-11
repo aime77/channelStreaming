1a1575b54f04bdd3cac456e4db2b095367e410bd
Jeme <aime.urquieta@outlook.com>
2019-03-10 21:57:58 -0500

    created protal for delete

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,4 @@
-import history from '../history'; 
+import history from "../history";
 
 import {
   SIGN_IN,
@@ -48,6 +48,8 @@ export const deleteStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
+
+  history.push("/");
 };
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Modal from "../Modal";
 
-const StreamDelete=()=>{
-return <div>StreamDelete</div>
-}
+const StreamDelete = () => {
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
+};
 
 export default StreamDelete;
